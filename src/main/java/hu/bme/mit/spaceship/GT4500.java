568de424cc6e9e0c683b6eb1f0eb11e76b12e2c0
Peti <buga.peti@gmail.com>
2023-09-27 17:19:32 +0200

    merge conflict test

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -85,6 +85,8 @@ public class GT4500 implements SpaceShip {
           wasPrimaryFiredLast = true;
         }
 
+        //branch B
+
         if (! secondaryTorpedoStore.isEmpty()) {
           firingSuccess = secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
